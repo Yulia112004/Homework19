ca8d35f39a25e8937bde1a8851b541eb5458fa84
Yulia Byckova <byckovaulia669@gmail.com>
2024-01-15 14:11:30 +0300

    Homework 20_1 Done

--- Homework19/catalog/models.py
+++ Homework19/catalog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='наименование')
+    disc = models.CharField(max_length=200, verbose_name='описание')
+
+    def __str__(self):
+        return  f'{self.category_name} ({self.disc})'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='наименование')
+    product_disc = models.CharField(max_length=200, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за штуку')
+    data_created = models.DateTimeField(verbose_name='дата создания')
+    data_changed = models.DateTimeField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.product_name}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+
+class Contact(models.Model):
+    country = models.CharField(max_length=100)
+    inn = models.CharField(max_length=12)
+    address = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.country} ({self.address})"
+
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
--- Homework19/catalog/views.py
+++ Homework19/catalog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from catalog.models import Product, Contact
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'products': Product.objects.all()[:6],
+    }
+    print(context)
+    return render(request, "main/index.html", context=context)
 
 def contacts(request):
+    context = {
+        'title': 'Contacts',
+        'adress_info': Contact.objects.first(),
+    }
     if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+        visiter = dict()
+        visiter['name'] = request.POST.get('name', None)
+        visiter['phone'] = request.POST.get('phone', None)
+        visiter['message'] = request.POST.get('message', None)
+        print(visiter)
 
-    return render(request, 'main/contacts.html')
+    return render(request, "main/contacts.html", context=context)
